367a2535d1fc35dadcc8bc4f7c7121e2f48b1a8c
Elvan Selvano <elvanselvano@yahoo.com>
2020-09-18 23:29:57 +0700

    implement zomato api

--- src/App.js
+++ src/App.js
@@ -1,33 +1,133 @@
-import React from "react";
-// import logo from './logo.svg';
-// import './App.css';
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import RestaurantCard from "./components/RestaurantCard";
-import restaurants from "./components/RestaurantData";
+// import restaurants from "./components/RestaurantData";
 import "./css/RestaurantCard.css";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { getCities, searchRestaurants } from "./api";
+import { parseCitySuggestions, parseSearchRestaurants } from "./utils";
+import CitySuggestions from "./city-suggestions";
+import Restaurants from "./restaurants";
+
+import "./css/Header.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import DirectionsIcon from "@material-ui/icons/Directions";
+import { YoutubeSearchedFor } from "@material-ui/icons";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [cityQuery, setCityQuery] = useState("Jakarta");
+  const [citySuggestions, setCitySuggestions] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+  const [error, setError] = useState();
+
+  function handleClick(event) {
+    setCityQuery(input);
+    event.preventDefault();
+  }
+
+  function handleChange(event) {
+    console.log(input);
+    setInput(event.target.value);
+  }
+
+  useEffect(() => {
+    const searchRestaurantsFromCity = async () => {
+      try {
+        const cities = parseCitySuggestions(await getCities(cityQuery));
+        setCitySuggestions(cities);
+
+        if (cities.length > 0) {
+          const restaurants = parseSearchRestaurants(
+            await searchRestaurants(cities[0].id)
+          );
+          setRestaurants(restaurants);
+        }
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    searchRestaurantsFromCity();
+  }, [cityQuery]);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      padding: "2px 4px",
+      display: "flex",
+      alignItems: "center",
+      width: 400,
+    },
+    input: {
+      marginLeft: theme.spacing(1),
+      flex: 1,
+    },
+    iconButton: {
+      padding: 10,
+    },
+    divider: {
+      height: 28,
+      margin: 4,
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <div>
-      <Header />
-      <Navigation />
-      {/* <div className="container"> */}
-      <div className="items">
-        {restaurants.map((restaurant) => (
-          <RestaurantCard
-            key={restaurant.id}
-            imageURL={restaurant.imageURL}
-            title={restaurant.title}
-            cuisine={restaurant.cuisine}
-            budget={restaurant.budget}
-            rating={restaurant.rating}
-          />
-        ))}
+      {error && <div>Error: {error}</div>}
+      {/* <Header /> */}
+      {/* INPUT FIELD */}
+      <div>
+        <div class="background__image">
+          <h1 className="header__title">GoZomato</h1>
+          {/* <Navigation /> */}
+          <div className="nav">
+            <Paper
+              component="form"
+              className={classes.root}
+              onSubmit={handleClick}
+            >
+              <InputBase
+                value={input}
+                className={classes.input}
+                placeholder="Search for City"
+                inputProps={{ "aria-label": "Search for City" }}
+                onChange={handleChange}
+              />
+              <IconButton
+                type="submit"
+                className={classes.iconButton}
+                aria-label="search"
+              >
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </div>
+        </div>
       </div>
+
+      <div>
+        <div className="items">Restaurants in {cityQuery}:</div>
+      </div>
+      {citySuggestions.length > 0 && (
+        <div className="container">
+          <div className="items">
+            {restaurants.map((e) => (
+              <RestaurantCard key={e.id} data={e} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-    // </div>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,34 +1,87 @@
-import React from "react";
-import TextInput from "./TextInput";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
+// import React from "react";
+// import TextInput from "./TextInput";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Button from "@material-ui/core/Button";
 import "../css/Navigation.css";
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     "& > *": {
+//       margin: theme.spacing(1),
+//     },
+//   },
+// }));
+
+// function Navigation() {
+//   const classes = useStyles();
+
+//   return (
+//     <div className={classes.root}>
+//       <div className="nav">
+//         <div className="nav__item">
+//           <TextInput />
+//         </div>
+//         <div className="nav__item">
+//           <Button variant="contained" color="primary">
+//             Change City
+//           </Button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Navigation;
+
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import DirectionsIcon from "@material-ui/icons/Directions";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    "& > *": {
-      margin: theme.spacing(1),
-    },
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
   },
 }));
 
-function Navigation() {
+export default function CustomizedInputBase() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className="nav">
-        <div className="nav__item">
-          <TextInput />
-        </div>
-        <div className="nav__item">
-          <Button variant="contained" color="primary">
-            Change City
-          </Button>
-        </div>
-      </div>
+    <div className="nav">
+      <Paper component="form" className={classes.root}>
+        <InputBase
+          className={classes.input}
+          placeholder="Search for City"
+          inputProps={{ "aria-label": "Search for City" }}
+        />
+        <IconButton
+          type="submit"
+          className={classes.iconButton}
+          aria-label="search"
+        >
+          <SearchIcon />
+        </IconButton>
+      </Paper>
     </div>
   );
 }
-
-export default Navigation;
